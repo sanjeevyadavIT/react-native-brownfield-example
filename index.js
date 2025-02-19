d4e9ed691243dff95eedb4617cf9f4aa30697007
Sanjeev Yadav <sanjeevy133@gmail.com>
2025-02-19 22:53:54 +0530

    Add dummy code push

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-import { Image } from "react-native";
+import React from "react";
 
-const FastImage = Image;
+const SyncStatus = {
+  CHECKING_FOR_UPDATE: "CHECKING_FOR_UPDATE",
+  DOWNLOADING_PACKAGE: "DOWNLOADING_PACKAGE",
+  INSTALLING_UPDATE: "INSTALLING_UPDATE",
+  UP_TO_DATE: "UP_TO_DATE",
+  UPDATE_INSTALLED: "UPDATE_INSTALLED",
+  UNKNOWN_ERROR: "UNKNOWN_ERROR",
+};
 
-FastImage.resizeMode = {
-    contain: 'contain',
-    cover: 'cover', // Default
-    stretch: 'stretch',
-    center: 'center',
+const CodePush = (config) => (AppComponent) => {
+  return function WrappedComponent(props) {
+    return <AppComponent {...props} />;
   };
-  
+};
 
-export default FastImage;
+CodePush.sync = () => {
+  console.log("CodePush.sync() called");
+};
+
+CodePush.checkForUpdate = () => {
+  console.log("CodePush.checkForUpdate() called");
+  return Promise.resolve(null);
+};
+
+CodePush.installUpdate = () => {
+  console.log("CodePush.installUpdate() called");
+};
+
+CodePush.getUpdateMetadata = async (updateState) => {
+  console.log(`CodePush.getUpdateMetadata(${updateState}) called`);
+  return Promise.resolve(null);
+};
+
+CodePush.SyncStatus = SyncStatus;
+CodePush.CheckFrequency = {};
+CodePush.InstallMode = {};
+UpdateState.UpdateState = {};
+
+export default CodePush;
+export const { sync, checkForUpdate, installUpdate } = CodePush;
